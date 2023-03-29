699e19de2721f8df92703307ec0dfbb4a6d592b5
Anatoly Semenov <anatolysrmenov@gmail.com>
2023-03-29 16:58:44 +0300

    Init microservice user-service

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['localhost:9092'],
+        },
+        consumer: {
+          groupId: 'user-consumer',
+        },
+      },
+    },
+  );
 
-  const configService = app.get(ConfigService);
-
-  await app.listen(configService.get<number>('port'));
+  await app.listen();
 }
 bootstrap();
